bcf097cb8eed5ced4d8d8eabc848057cb22502c6
Abigail Arisa <abigailarisa1@gmail.com>
2022-08-31 14:57:54 -0500

    commit tree 563e9e9c7322bd18ed5c56e2e13a6712d2884939

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+// function multiply() {
+
+//     return num1 * 2;
+//   }
+//   multiply();
+
+// const
+
+const multiply = (num1 * num2)
+const random = 1
+Math.floor(Math.random() * 10) + 1
+
+const num3 = 25
+const num4 = 7
+const mod = num3 % num4
+
+const max = 20
+Math.max(2, 5, 20)
